4a3cb763b044d616f16e016aab900a7dbfb08c1a
xm286947-cell <xm286947@gmail.com>
2026-09-22 20:01:58 +0800

    feat(storage): export semantic second-pass coordinator

--- storage_e2e/__init__.py
+++ storage_e2e/__init__.py
@@ -4,10 +4,20 @@ from .json_truncation import (
     ProviderResponse,
 )
 from .real_provider import OpenAICompatibleStorageRealProvider
+from .semantic_second_pass import (
+    PRIMARY_AGENT_ID,
+    SECOND_PASS_AGENT_ID,
+    StorageSemanticSecondPassCoordinator,
+    StorageSemanticSecondPassOutcome,
+)
 
 __all__ = [
     "E2ERunResult",
     "JsonTruncationAwareStorageAdapter",
     "ProviderResponse",
     "OpenAICompatibleStorageRealProvider",
+    "PRIMARY_AGENT_ID",
+    "SECOND_PASS_AGENT_ID",
+    "StorageSemanticSecondPassCoordinator",
+    "StorageSemanticSecondPassOutcome",
 ]
