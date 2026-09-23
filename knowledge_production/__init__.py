eab1d21832817cbbf9d48a5c1e782edea7397963
xm286947-cell <xm286947@gmail.com>
2026-09-23 15:02:00 +0800

    feat(kp-d01): export business candidate intake API

--- knowledge_production/__init__.py
+++ knowledge_production/__init__.py
@@ -1,19 +1,28 @@
 from .candidate import (
     KnowledgeCandidateError,
     KnowledgeCandidateService,
+    business_source_ref_key,
     source_ref_key,
 )
 from .extraction import (
     KnowledgeExtractionError,
     KnowledgeExtractionService,
 )
+from .intake import (
+    BusinessCandidateIntakeError,
+    BusinessCandidateIntakeService,
+)
 from .models import (
+    BusinessCandidateInput,
+    BusinessSourceType,
+    CandidateSourceType,
     CandidateStatus,
     EvidenceLocation,
     KnowledgeCandidate,
     KnowledgeExtractionCandidateDraft,
     KnowledgeExtractionOutput,
     KnowledgeObjectType,
+    KNOWLEDGE_CANDIDATE_CONTRACT_VERSION,
     RetrievalStatus,
     SourceDocument,
     SourceStatus,
@@ -23,6 +32,11 @@ from .models import (
 from .source_document import SourceDocumentError, SourceDocumentService
 
 __all__ = [
+    "BusinessCandidateInput",
+    "BusinessCandidateIntakeError",
+    "BusinessCandidateIntakeService",
+    "BusinessSourceType",
+    "CandidateSourceType",
     "CandidateStatus",
     "EvidenceLocation",
     "KnowledgeCandidate",
@@ -31,8 +45,10 @@ __all__ = [
     "KnowledgeExtractionError",
     "KnowledgeExtractionService",
     "KnowledgeObjectType",
+    "KNOWLEDGE_CANDIDATE_CONTRACT_VERSION",
     "KnowledgeCandidateError",
     "KnowledgeCandidateService",
+    "business_source_ref_key",
     "source_ref_key",
     "RetrievalStatus",
     "SourceDocument",
