0408ba4796973fb0d1a5305527c30ffa15c85560
xm286947-cell <xm286947@gmail.com>
2026-09-24 13:25:19 +0800

    HC package runtime: redirect legacy shell launcher

--- run_hardware_case_product_test.sh
+++ run_hardware_case_product_test.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
 set -eu
 cd "$(dirname "$0")"
-echo "Hardware Case Product Test V0.1"
-echo "P07: http://127.0.0.1:8080/p0/hardware-cases/base-data"
-python main.py knowledge-p1-start --db knowledge/quality_capability_p1.db --host 127.0.0.1 --port 8080
+exec sh START_HARDWARE_CASE.sh
